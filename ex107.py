d55feace7cd34db2e349501d183894d4d6f3ea1f
Alan de Sant'Anna <108240669+alandesantanna@users.noreply.github.com>
2024-01-14 11:23:22 -0300

    Initial commit

---
+++ ex107.py
@@ -0,0 +1,6 @@
+import moduloEx107
+p = float(input('Digite o preço: R$'))
+print(f' Metade de {p} é {moduloEx107.metade(p)}')
+print(f'O dobro de {p} é {moduloEx107.dobro(p)}')
+print(f'Aumentando 10%, temos {moduloEx107.aumenta(p)}')
+print(f'Reduzindo 13%, temos {moduloEx107.reduzindo(p)}')
---
+++ moduloEx107.py
@@ -0,0 +1,19 @@
+def metade(n):
+    return n/2
+
+
+def dobro(n):
+    return n*2
+
+
+def aumenta(n):
+    n1 = n/100
+    valor = (n1 * 10) + n
+    return valor
+
+
+def reduzindo(n):
+    n1 = n/100
+    valor = n - (n1 * 13)
+    return valor
+
